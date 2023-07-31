340963acfac5c9ab5470f2b8a52b75d7fd3a96d7
osmanzakaria532 <osmanzakaria179@gmail.com>
2023-08-01 00:14:16 +0600

    Valentina Website

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$('.testimonial_sliders').slick({
+    slidesToShow: 2,
+    autoplay: true,
+    speed: 300,
+    arrows: true,
+    prevArrow: '<a href="#" class="left_arrow"><i class="fa-solid fa-angle-left"></i></a>',
+    nextArrow: '<a href="#" class="right_arrow"><i class="fa-solid fa-angle-right"></i></a>',
+});
